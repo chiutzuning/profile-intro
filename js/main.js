ba6409391b34973e3e4168087b5b39290cc9f64e
Tzuning Chiu <chiutzuning@gmail.com>
2022-03-05 20:34:48 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  let $btns = $('.project-area .button-group button');
+
+  $btns.click(function(event){
+    $('.project-area .button-group button').removeClass('active');
+    event.target.classList.add('active');
+
+    let selector = $(event.target).attr("data-filter");
+    $('.project-area .grid').isotope({
+      filter: selector
+    });
+
+    return false;
+  })
+
+  $('.project-area .button-group #btns')
+
+});
